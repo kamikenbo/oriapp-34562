cf7657603795975abb0473a5de706059676a304d
kamikenbo <kenta_kamio@icloud.com>
2021-05-03 00:30:22 +0900

    account編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
     @nickname = user.nickname
     @tweets = user.tweets.order('created_at DESC')
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :baby_name, :baby_birthday)
+  end
+  
 end
